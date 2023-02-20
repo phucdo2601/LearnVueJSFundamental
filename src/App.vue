2a17897632edfb85f02dc54bd32c69d229155d85
Phuc Do <ngocphucdo2601@gmail.com>
2023-02-20 22:48:18 +0700

    First learn basic databiding with form b02

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <QuizForm />
+</template>
+
+<script>
+  import QuizForm from "./components/QuizForm.vue";
+
+  export default {
+    name: "App",
+    components: {
+      QuizForm
+    }
+  }
+</script>
