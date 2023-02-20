77da7ec06ab3adc11fdcf43e5abc99cec2bbcf5d
Phuc Do <ngocphucdo2601@gmail.com>
2023-02-20 15:37:35 +0700

    First create vuejs 3 project by npm b01

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: "HeaderComp",
+    data() {
+        return {
+            
+        }
+    },
+
+    methods: {
+        
+    },
+}
+</script>
+
+<template>
+    <h1>This is a header component</h1>
+</template>
+
+<style></style>
