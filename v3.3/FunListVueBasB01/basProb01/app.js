88c7ececa548f8e44b33907db2987fd6f887df40
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-10-31 13:29:49 +0700

    [learn-bas-vuejs-syntax-2023] 05. Basic learn Listening to Events b01

--- v3.3/FunListVueBasB01/basProb01/app.js
+++ v3.3/FunListVueBasB01/basProb01/app.js
@@ -3,12 +3,20 @@ const vm =  Vue.createApp({
         return {
             firstName: 'John',
             lastName: "Billy",
-            url: "https://www.google.com.vn/"
+            url: "https://www.google.com.vn/",
+            raw_url: '<a href="https://www.google.com.vn/" target="_blank">Google</a>',
+            age: 23,
         }
     },
     methods: {
         fullName() {
             return `${this.firstName} ${this.lastName.toUpperCase()}`;
+        },
+        increment() {
+            this.age++;
+        },
+        updateLastName(event) {
+            this.lastName = event.target.value;
         }
     }
 }).mount('#app');
