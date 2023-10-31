450910f7c20a9f919e39b608e5d9341f553f7b87
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-10-31 09:50:39 +0700

    [learn-bas-vuejs-syntax-2023] 02. Learn basic method in vuejs b01

--- v3.3/FunListVueBasB01/basProb01/app.js
+++ v3.3/FunListVueBasB01/basProb01/app.js
@@ -1,8 +1,19 @@
-Vue.createApp({
+const vm =  Vue.createApp({
     data() {
         return {
             firstName: 'John',
             lastName: "Billy" 
         }
+    },
+    methods: {
+        fullName() {
+            return `${this.firstName} ${this.lastName.toUpperCase()}`;
+        }
     }
 }).mount('#app');
+
+// setTimeout(() => {
+//     vm.firstName = 'Bob';
+// }, 2000)
+
+
